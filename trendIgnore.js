0a6a070dfd90169f7e7cc1355b8f5deb1223e7a9
gensobunya <gen@gensobunuya.net>
2018-03-08 20:22:24 +0900

    new feature: リアルタイムに設定更新を監視

--- trendIgnore.js
+++ trendIgnore.js
@@ -4,13 +4,19 @@
 const qiitaBaseUrl = "https://qiita.com/";
 
 chrome.storage.local.get("redirectPage", (items)=>{
+	let redirectFullUrl = qiitaBaseUrl + items.redirectPage;
 	chrome.webRequest.onBeforeRequest.addListener( (detail) =>{
-		return {redirectUrl : qiitaBaseUrl + items.redirectPage};
+		return {redirectUrl : redirectFullUrl};
 	},
 		{urls: ["*://qiita.com/trend"]}, //リクエスト先がtrendになった場合発火
 		["blocking"]
 	)
 	console.log("redirect to "+ qiitaBaseUrl + items.redirectPage);
+
+	chrome.storage.onChanged.addListener((newItems)=>{
+		redirectFullUrl = qiitaBaseUrl + newItems.redirectPage.newValue;
+		console.log("change redirect url to" + redirectFullUrl);
+	})
 }
 )
 
